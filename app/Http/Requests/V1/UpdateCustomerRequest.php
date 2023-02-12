8ff706bd2e7134cece68a0fc662dd30266bf863d
Fredrick Ochieng <flaughters@gmail.com>
2023-02-12 17:21:55 +0300

    PUT & PATCH method sorted😎

---
+++ app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+
+        $method = $this->method();
+
+        if ($method == 'PUT') {
+
+            return [
+                'name' => ['required'],
+                'type' => ['required', Rule::in(['I', 'B', 'i', 'b'])],
+                'email' => ['required', 'email'],
+                'address' => ['required'],
+                'city' => ['required'],
+                'state' => ['required'],
+                'zip' => ['required'],
+            ];
+        } else {
+            return [
+                'name' => ['sometimes','required'],
+                'type' => ['sometimes','required', Rule::in(['I', 'B', 'i', 'b'])],
+                'email' => ['sometimes','required', 'email', Rule::unique('customers')->ignore($this->customer)],
+                'address' => ['sometimes','required'],
+                'city' => ['sometimes','required'],
+                'state' => ['sometimes','required'],
+                'zip' => ['sometimes','required'],
+            ];
+        }
+    }
+}
