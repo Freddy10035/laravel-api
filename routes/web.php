6be807d79888cc2379be263e8e70f5514adcebb1
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-03-17 13:48:46 +0300

    implemented sanctum autentication to the web.api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\MyUserModel;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +18,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get ('/setup', function(){
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password',
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        //$user = new \App\Models\User();
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if(Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-token',['create', 'update', 'delete']);
+            $updateToken = $user->createToken('update-token',['create', 'update']);
+            $basicToken = $user->createToken('basic-token');
+
+            return response()->json([
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken,
+            ]);
+            
+        }
+    }
+
 });
